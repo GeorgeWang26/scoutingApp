8945cd3894442b66ca5aeb167d5ef4acd3864b37
Taylor Xi <txi1@ocdsb.ca>
2020-01-11 15:16:58 -0500

    More UI changes

--- ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
+++ ScoutingApp/app/src/main/java/com/example/scoutingapp/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.nav_home)
                 .setDrawerLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
